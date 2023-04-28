b879dce591ea08341d59a0ce50b34dbd4a305ca1
aligorbor <aligorbor@list.ru>
2023-04-28 12:21:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/olimpiafitness/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.olimpiafitness
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.olimpiafitness.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
